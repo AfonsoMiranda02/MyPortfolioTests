653fc4430313b07459124c9d929473924ad457e8
AfonsoMiranda02 <afonsorm4321@gmail.com>
2025-02-23 16:37:52 +0000

    Added Utils With All tags Built

---
+++ includes/utils/tags.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="css/style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body class="tagsBody">
+    <h1 class="TagsTitle">Tags</h1>
+    <!-- Copy Paste Tags -->
+    <div class="tagsPos">
+        <!-- Green -->
+        <button class="greenTagBig"><p class="greenCircleBig"></p>Completed</button><!-- Big -->
+        <button class="greenTagSmall"><p class="greenCircleSmall"></p>Completed</button><!-- Small -->
+        <!-- Yellow -->
+        <button class="yellowTagBig"><p class="yellowCircleBig"></p>Paused</button><!-- Big -->
+        <button class="yellowTagSmall"><p class="yellowCircleSmall"></p>Paused</button><!-- Small -->
+        <!-- Blue -->
+        <button class="blueTagBig"><p class="blueCircleBig"></p>On Going</button><!-- Big -->
+        <button class="blueTagSmall"><p class="blueCircleSmall"></p>On Going</button><!-- Small -->
+        <!-- Red -->
+        <button class="redTagBig"><p class="redCircleBig"></p>Stopped</button><!-- Big -->
+        <button class="redTagSmall"><p class="redCircleSmall"></p>Stopped</button><!-- Small -->
+    </div>
+</body>
+</html>
